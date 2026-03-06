eebeecd76cab5df22de61221c1ec594d4138e87b
pyoyujin220-sketch <pyoyujin220@gmail.com>
2026-03-06 20:46:09 +0900

    ;;

--- Assets/Scripts/GameUIManager.cs
+++ Assets/Scripts/GameUIManager.cs
@@ -2,30 +2,42 @@ using UnityEngine;
 
 public class GameUIManager : MonoBehaviour
 {
-    public GameObject ToggleCanvas;
+    public GameObject SettingCanvas;
+    public GameObject gameoverCanvas;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if(gameoverCanvas!=null)
+        {
+            gameoverCanvas.SetActive(false);
+        }
     }
 
     public void ActiveUI()
     {
-        if (ToggleCanvas != null)
+        if (SettingCanvas != null)
         {
 
-            ToggleCanvas.SetActive(true);
+            SettingCanvas.SetActive(true);
         }
     }
 
     public void DeActiveUI()
     {
-        if (ToggleCanvas != null)
+        if (SettingCanvas != null)
         {
-            ToggleCanvas.SetActive(false);
+            SettingCanvas.SetActive(false);
         }
     }
 
+    public void GameOver()
+    {
+        if(gameoverCanvas!=null)
+        {
+            Time.timeScale = 0;
+            gameoverCanvas.SetActive(true);
+        }
+    }
 
     // Update is called once per frame
     void Update()
